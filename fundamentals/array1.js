173e74f1e6f6463b3238c5a05ac95f05796a096a
umesh raut <uraut6044@gmail.com>
2025-07-05 14:15:38 +0530

    for each

--- fundamentals/array1.js
+++ fundamentals/array1.js
@@ -1,5 +1,15 @@
 let chaiTypes = ["Masala chai", "ginger tea", "Darjeeling Tea", "Noon Tea"];
 
-chaiTypes.forEach((chai) => {
-    console.log(chai);
+// chaiTypes.forEach((chai) => {
+//     console.log(chai);
+// })
+
+// spred
+
+let newChai = ["Oolong Tea", "Chamolina Tea", "Nilgiri Tea"];
+
+let allChai = [...chaiTypes, ...newChai];
+
+allChai.forEach((chai, index) => {
+    console.log(`${index + 1}: ${chai}`);
 })
