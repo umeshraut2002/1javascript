708985fa1566938f34877982ad1cd0b2cb9bd7ad
umesh raut <uraut6044@gmail.com>
2025-07-06 13:26:09 +0530

    map

---
+++ javascript-core/map-filter-reduce/map.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers and returns an array of their squares using map.
+
+let number = [1, 2, 3, 4, 5, 6];
+
+function squareMethd(number){
+    return number.map((ele) => ele * ele);
+}
+
+console.log(squareMethd(number));
