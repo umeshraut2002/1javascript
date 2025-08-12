ba33b7063e938fe49149ceae8c2954618b2a6ab5
umesh raut <uraut6044@gmail.com>
2025-08-12 17:02:38 +0530

    class object person

---
+++ object_xchema.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(){
+        this.name = "Vihaan",
+        this.age = 24,
+        this.location = "Nagpur"
+    }
+}
+
+const obj = new Person();
+
+console.log(obj)
