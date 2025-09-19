72c5e9897714de71d5b2b86adbb43fa093128cd7
umesh raut <uraut6044@gmail.com>
2025-09-19 14:32:00 +0530

    mew challenge intiated

---
+++ 10daysJavascriptChallenge.js/01Challenge.js
@@ -0,0 +1,18 @@
+/*
+2–3 hours
+
+Parse JSON data from a mock API response.
+
+Convert an array of objects into different structures (e.g., group by category, sort by date).
+
+Write a function to flatten nested objects.
+
+Scenario: Build a utility that takes e-commerce product data and outputs reports like:
+
+Products grouped by category
+
+Cheapest product per category
+
+Top 3 most expensive products
+*/
+
