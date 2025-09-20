266e9d80ae034dfd0f7407025b3361461245b343
umesh raut <uraut6044@gmail.com>
2025-09-20 14:00:17 +0530

    filter intiated

--- 10daysJavascriptChallenge.js/map_filter_reduce.js
+++ 10daysJavascriptChallenge.js/map_filter_reduce.js
@@ -6,4 +6,32 @@ const newNums = myNumbers.filter( num => {
     return num > 4;
 })
 
-console.log(newNums);
+console.log(newNums); 
+
+// exercise 1 
+
+// const users = [
+//   { id: 1, name: "Amit", active: true },
+//   { id: 2, name: "Sneha", active: false },
+//   { id: 3, name: "Rahul", active: true },
+//   { id: 4, name: "Priya", active: false }
+// ];
+
+// const activeUsers = users.filter( user => {
+//     return user.active;
+// })
+
+// console.log(activeUsers);
+
+const products = [
+  { name: "Laptop", price: 55000 },
+  { name: "Mouse", price: 500 },
+  { name: "Keyboard", price: 1500 },
+  { name: "Book", price: 300 }
+];
+
+const higherPriceProducts =products.filter( product => {
+    return product.price > 500;
+})
+
+console.log(higherPriceProducts);
